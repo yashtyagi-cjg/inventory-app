66728666de8d4eedb9bf3e137a6d15baf7436ca4
yashtyagi-cjg <s2014.moose@gmail.com>
2024-05-12 02:36:55 +0530

    Reverting changes for vercel deployment

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var compression = require('compression');
+const base64vlq = require('./base64-vlq.js');
+
 var helmet = require('helmet');
 
 mongoose.set("strictQuery", false);
